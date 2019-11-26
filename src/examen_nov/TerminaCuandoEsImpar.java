f2db468ee7e37eebecdc8c786ba833073febdc19
driy <driy@nigul.cide.es>
2019-11-26 16:02:17 +0100

    Commit ejercicio 8

--- src/examen_nov/TerminaCuandoEsImpar.java
+++ src/examen_nov/TerminaCuandoEsImpar.java
@@ -1,9 +1,5 @@
-/**
- * Bucle do-while que termina cuando se introduce por teclado un
- * número impar.
- *
- * @author Luis José Sánchez
- */
+package examen_nov;
+
 public class TerminaCuandoEsImpar {
   public static void main(String[] args) {
     int numero;
@@ -12,7 +8,7 @@ public class TerminaCuandoEsImpar {
       System.out.print("Dime un número: ");
       numero = Integer.parseInt(System.console().readLine());
       
-      if (numero % 2 == 0) {// comprueba si el número introducido es par
+      if (numero % 2 == 0) {
         System.out.println("Qué bonito es el " + numero);
       } else {
         System.out.println("No me gustan los números impares, adiós.");
